fdea583f48a94c427dab1ced8d8fba5cfcefe9cf
Adejare Toluwalope <standardblaise@gmail.com>
2021-08-25 22:42:20 +0100

    Creating user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'naomi1@gmail.com', password: 'naomi2', password_confirmation: 'naomi2')
+
 30.times do
     Course.create!([{
         title: Faker::Educator.course_name,
-        description: Faker::TvShows::GameOfThrones.quote
+        description: Faker::TvShows::GameOfThrones.quote,
+        user_id: User.first.id
     }])
 end
