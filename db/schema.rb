f8a233350e270534dbfddaa2ba5119254e856af6
Adejare Toluwalope <standardblaise@gmail.com>
2021-08-27 00:43:25 +0100

    Creating course card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_195213) do
+ActiveRecord::Schema.define(version: 2021_08_23_215106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,6 @@ ActiveRecord::Schema.define(version: 2021_08_24_195213) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,5 +66,4 @@ ActiveRecord::Schema.define(version: 2021_08_24_195213) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "courses", "users"
 end
