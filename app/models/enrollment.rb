6ea78e5091c245c6c192514dac9aca2bc774289e
Adejare Toluwalope <standardblaise@gmail.com>
2021-09-11 03:14:40 +0100

    Creating enrollment table

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -7,14 +7,20 @@ class Enrollment < ApplicationRecord
   validates_uniqueness_of :user_id, scope: :course_id #user can't be suscribed to the same course twice
   validates_uniqueness_of :course_id, scope: :user_id #user can't be suscribed to the same course twice
 
-  validate cant_suscribe_to_own_course #user can't create suscribtion if course.user == current_user.id
+  validate :cant_suscribe_to_own_course #user can't create suscribtion if course.user == current_user.id
 
-  def cant_suscribe_to_own_course
-    if self.new_record?
-        if user_id == course.user_id
-            errors.add(:base "You can't suscribe to your own course")
+  def to_s
+    user.to_s + " " + course.to_s
+  end
+  protected
+    def cant_suscribe_to_own_course
+        if self.new_record?
+            if self.user_id.present?
+                if user_id == course.user_id
+                    errors.add(:base, "You can't suscribe to your own course")
+                end
+            end
         end
     end
-  end
 
 end
