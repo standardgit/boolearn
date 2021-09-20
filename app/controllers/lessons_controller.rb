62eeeafcbd2d062fb39b3b5e6bf4f397b602b4b1
Adejare Toluwalope <standardblaise@gmail.com>
2021-09-20 23:41:23 +0100

    routes and style

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -9,6 +9,7 @@ class LessonsController < ApplicationController
   def show
     authorize @lesson
     current_user.view_lesson(@lesson)
+    @lessons = @course.lessons
   end
 
   # GET /lessons/new
