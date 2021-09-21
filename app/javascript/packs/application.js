5e183db26fbe14222a2e6daf0b82e1c87faba849
Adejare Toluwalope <standardblaise@gmail.com>
2021-09-21 23:33:39 +0100

    Adding analytics page and routes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,8 @@ require("@rails/actiontext")
 // require("@fortawesome/fontawesome-free/css/all")
 import "@fortawesome/fontawesome-free/css/all"
 
+import "chartkick/chart.js"
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
