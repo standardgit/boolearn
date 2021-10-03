0b24c85d56386f42a008a4cf6f86571701e19c62
Adejare Toluwalope <standardblaise@gmail.com>
2021-10-03 16:34:54 +0100

    Adding ranks to lessons

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -25,7 +25,8 @@ class CoursePolicy < ApplicationPolicy
   end
 
   def destroy?
-    @user.has_role?(:admin) || @record.user == @user
+    @record.user == @user && @record.enrollments.none?
+    #@user.has_role?(:admin) || @record.user == @user
   end
 
   def approve?
@@ -38,5 +39,9 @@ class CoursePolicy < ApplicationPolicy
 
   def owner?
     @record.user == @user
+  end
+
+  def admin_or_owner?
+    @user.has_role?(:admin) || @record.user == @user
   end
 end
---
+++ db/migrate/20211003150021_add_order_to_lessons.rb
@@ -0,0 +1,5 @@
+class AddOrderToLessons < ActiveRecord::Migration[6.0]
+  def change
+    add_column :lessons, :row_order, :integer
+  end
+end
