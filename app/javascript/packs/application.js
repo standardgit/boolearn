32a8db3e6391fc4230ca8291c920a50a7c96927d
standardgit <standardblaise@gmail.com>
2021-11-04 13:13:08 +0100

    Adding preview to blobs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ require("jquery-ui-dist/jquery-ui")
 import "@fortawesome/fontawesome-free/css/all"
 
 import "chartkick/chart.js"
+import "../trix-editor-overrides"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
