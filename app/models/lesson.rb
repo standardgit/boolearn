65eb5ed929510398117cd0000edb5c20b2aba32a
standardgit <standardblaise@gmail.com>
2021-11-05 22:46:24 +0100

    Adding Videos to our lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,14 @@ class Lesson < ApplicationRecord
 
   validates :title, :content, :course, presence: true
   validates :title, uniqueness: true, length: { :maximum => 70 }
+  validates :video,
+    content_type: [ 'video/mp4' ], 
+    size: { less_than: 50.megabytes , message: 'video size should be under 50 megabytes' }
+
+  validates :video_thumbnail,
+    content_type: ['image/png', 'image/jpg', 'image/jpeg', 
+    size: { less_than: 500.kilobytes , message: 'video size should be under 500 kilobytes' }
+
   belongs_to :course, counter_cache: true
   #Course.find_each { |course| Course.reset_counters(courses.id, :lessons) }
   has_many :user_lessons, dependent: :destroy
@@ -10,6 +18,8 @@ class Lesson < ApplicationRecord
   friendly_id :title, use: :slugged
 
   has_rich_text :content
+  has_one_attached :video
+  has_one_attached :video_thumbnail
 
   include RankedModel
   ranks :row_order, :with_same => :course_id
