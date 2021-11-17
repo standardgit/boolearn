6a98a91ef52377463b271386e32cad90528bee1b
standardgit <standardblaise@gmail.com>
2021-11-17 22:45:44 +0100

    Comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,20 @@
 class CommentsController < ApplicationController
 
     def create
+        @comment = Comment.new(comment_params)
+        @course = Course.friendly.find(param[:course_id])
+        @lesson = Lesson.friendly.find(params[:id])
+        @comment.lesson_id = @lesson.id
+
+        if @Comment.save
+            redirect_to course_lesson_path(@course, @lesson), notice: 'Lesson was successfully created.'
+        else
+            render :new
+        end
     end
 
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
 end
