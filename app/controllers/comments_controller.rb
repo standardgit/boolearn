4ac4eafbe9b73e9e0bdbe5118260289019124081
standardgit <standardblaise@gmail.com>
2021-11-18 22:50:38 +0100

    Render comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,23 @@ class CommentsController < ApplicationController
         @course = Course.friendly.find(param[:course_id])
         @lesson = Lesson.friendly.find(params[:id])
         @comment.lesson_id = @lesson.id
+        @comment.user = current_user
 
-        if @Comment.save
-            redirect_to course_lesson_path(@course, @lesson), notice: 'Lesson was successfully created.'
-        else
-            render :new
-        end
+        # if @Comment.save
+        #     redirect_to course_lesson_path(@course, @lesson), notice: 'Lesson was successfully created.'
+        # else
+        #     render :new
+        # end
+
+        respond_to do |format|
+            if @comments.save
+              format.html { redirect_to course_lesson_path(@course, @lesson), notice: "Lesson was successfully created." }
+              format.json { render :show, status: :created, location: @lesson }
+            else
+              format.html { render "lessons/comments/new" }
+              format.json { render json: @lesson.errors, status: :unprocessable_entity }
+            end
+          end
     end
 
     private
