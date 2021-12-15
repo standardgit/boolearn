ff2da50d0349af952940daf0f8b34771fa6f0add
standardgit <standardblaise@gmail.com>
2021-12-15 21:51:49 +0100

    Styling the the home page

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,6 @@
 #initialize recaptcha.rb
 Recaptcha.configure do |config|
+    config.site_key = Rails.application.credentials.dig(:recaptcha, :site_key)
     config.site_key  = '6Ld_e5MdAAAAAFn4c4d9RoEGmrAAs6rPx68PDcs8'
     config.secret_key = '6Ld_e5MdAAAAAF7niN7zwZ8Fz1wgMZ_f24Vv4uXA'
     #uncomment the following line if you are using a proxy
