2f7ca5e8da1d5715a6a97ca6f2502b9242fb9d9b
standardgit <standardblaise@gmail.com>
2022-01-02 23:54:18 +0100

    Adding validation to tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,7 @@ class Tag < ApplicationRecord
     has_many :course_tags
     has_many :courses, through: :course_tags
 
-    validates :name, length: (minimum: 1, maximum: 25), uniqueness: :true
+    
+  validates_uniqueness_of :name, length: (minimum: 1, maximum: 25)
 
 end
